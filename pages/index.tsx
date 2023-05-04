7336384bb9423e948802124515b5ad6335f96c61
shibushi233 <shishicscs994@gmail.com>
2023-05-04 12:54:39 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
 
         <div className="py-8 w-full flex items-center justify-center space-x-6">
           <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
-            <Link href="https://supabase.com" className="flex items-center justify-center">
+            <Link href="/" className="flex items-center justify-center">
               <p className="text-base mr-2">科易智能物联网</p>
               <Image src={'/supabase.svg'} width="20" height="20" alt="Supabase logo" />
             </Link>
